c9410a8192b6745d561d810ee5cd1c00e4ce114f
Hannah Mencer <hrm41@pitt.edu>
2022-11-02 16:38:01 -0400

    created ANOVA R file

---
+++ ANOVA-Test.R
@@ -0,0 +1,9 @@
+#setup
+library(ggplot2)
+library(ggpubr)
+library(tidyverse)
+library(broom)
+library(AICcmodavg)
+
+df=read.csv('C:\\Users\\Hanna\\Documents\\GitHub\\LRDC-Research\\CSV\\participant_averages.csv')
+head(df)
