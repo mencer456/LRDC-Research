6ce95ab051e101c560ef0ab4de4621d9bb7ce4a6
Hannah Mencer <hrm41@pitt.edu>
2022-11-03 22:21:48 -0400

    did ANOVA exploration

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -40,4 +40,42 @@ tukey.int
 
 plot=aov(text_len~as.factor(native_language):as.factor(level_id),data=test)
 plot.test=TukeyHSD(plot)
-plot.test
+plot(plot.test,las=1)
+
+#make dataframe with group labels
+mean.len.data=test%>%
+  group_by(native_language,level_id)%>%
+  summarise(
+    len=mean(text_len)
+  )
+
+mean.len.data$group=c('a','b','b','b','b','b','b','b','c')
+mean.len.data
+
+two.way.plot=ggplot(test,aes(x=native_language,y=text_len,group=level_id))+
+  geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
+
+two.way.plot
+#add standard err an means
+newtwo.way.plot <- two.way.plot +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2,color='maroon') +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange',color='maroon') +
+  geom_point(data=mean.len.data, aes(x=native_language, y=len,color='pink'))
+
+newtwo.way.plot
+facet=newtwo.way.plot+
+  facet_wrap(~level_id)
+facet
+
+rev.two.way.plot=ggplot(test,aes(x=level_id,y=text_len,group=native_language))+
+  geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
+
+rev.two.way.plot
+
+new.revtwo.way.plot <- rev.two.way.plot +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2,color='maroon') +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange',color='maroon') +
+  geom_point(data=mean.len.data, aes(x=level_id, y=len,color='pink'))+
+  facet_wrap(~native_language)
+
+new.revtwo.way.plot
