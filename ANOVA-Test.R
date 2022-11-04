d5629865606f71a5db00996a53277cb4241d9557
Hannah Mencer <hrm41@pitt.edu>
2022-11-03 22:38:22 -0400

    created graphics

--- ANOVA-Test.R
+++ ANOVA-Test.R
@@ -79,3 +79,53 @@ new.revtwo.way.plot <- rev.two.way.plot +
   facet_wrap(~native_language)
 
 new.revtwo.way.plot
+
+
+
+#DO IT AGAIN FOR WORD TYPE
+wt=aov(wordtype_len~as.factor(native_language)+as.factor(level_id),data=test)
+summary(wt)
+
+par(mfrow=c(2,2))
+plot(wt)
+par(mfrow=c(1,1))
+
+tukey.wt=TukeyHSD(wt)
+tukey.wt
+
+plot=aov(wordtype_len~as.factor(native_language):as.factor(level_id),data=test)
+plot.test=TukeyHSD(plot)
+plot(plot.test,las=1)
+
+mean.wt.data=test%>%
+  group_by(native_language,level_id)%>%
+  summarise(
+    wt=mean(wordtype_len)
+  )
+
+mean.len.data$group=c('a','b','b','b','b','b','b','b','c')
+mean.len.data
+
+twoway.wt=ggplot(test,aes(x=native_language,y=wordtype_len,group=level_id))+
+  geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
+twoway.wt
+
+wt.plot <- twoway.wt +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2,color='maroon') +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange',color='maroon') +
+  geom_point(data=mean.wt.data, aes(x=native_language, y=wt,color='pink'))+
+  facet_wrap(~level_id)
+
+wt.plot
+
+revtwoway.wt=ggplot(test,aes(x=level_id,y=wordtype_len,group=native_language))+
+  geom_point(cex = 1.5, pch = 1.0,position = position_jitter(w = 0.1, h = 0))
+revtwoway.wt
+
+rwt.plot <- revtwoway.wt +
+  stat_summary(fun.data = 'mean_se', geom = 'errorbar', width = 0.2,color='maroon') +
+  stat_summary(fun.data = 'mean_se', geom = 'pointrange',color='maroon') +
+  geom_point(data=mean.wt.data, aes(x=level_id, y=wt,color='pink'))+
+  facet_wrap(~native_language)
+
+rwt.plot
